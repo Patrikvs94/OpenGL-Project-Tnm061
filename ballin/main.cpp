e1829e63e16dda24ac2b7dbc21b9805af14b2e6c
Arohdin <adam.rohdin@outlook.com>
2016-04-11 11:49:04 +0300

    försökt till att fixa
    detta funkar inte så bra

--- ballin/main.cpp
+++ ballin/main.cpp
@@ -1,7 +1,101 @@
+// File and console I/O for logging and error reporting
 #include <iostream>
 
-using namespace std;
+#define GLFW_INCLUDE_GLU
 
-int main(){
+// In MacOS X, tell GLFW to include the modern OpenGL headers.
+// Windows does not want this, so we make this Mac-only.
+#ifdef __APPLE__
+#define GLFW_INCLUDE_GLCOREARB
+#endif
 
+// GLFW 3.x, to handle the OpenGL window
+#include <GLFW/glfw3.h>
+
+
+/*
+ * main(argc, argv) - the standard C++ entry point for the program
+ */
+int main(int argc, char *argv[]) {
+
+    using namespace std;
+
+	int width, height;
+
+    const GLFWvidmode *vidmode;  // GLFW struct to hold information about the display
+	GLFWwindow *window;    // GLFW struct to hold information about the window
+
+    // Initialise GLFW
+    /*
+    if(!glfwInit())
+    {
+        exit(EXIT_FAILURE);
+    }
+    */
+    glfwInit();
+
+    // Determine the desktop size
+    vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+	// Make sure we are getting a GL context of at least version 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Exclude old legacy cruft from the context. We don't need it, and we don't want it.
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // Open a square window (aspect 1:1) to fill half the screen height
+    window = glfwCreateWindow(vidmode->height/2, vidmode->width/2, "GLprimer", NULL, NULL);
+    if (!window)
+    {
+        cout << "Unable to open window. Terminating." << endl;
+        glfwTerminate(); // No window was opened, so we can't continue in any useful way
+        return -1;
+    }
+
+    // Make the newly created window the "current context" for OpenGL
+    // (This step is strictly required, or things will simply not work)
+    glfwMakeContextCurrent(window);
+
+    // Show some useful information on the GL context
+    cout << "GL vendor:       " << glGetString(GL_VENDOR) << endl;
+    cout << "GL renderer:     " << glGetString(GL_RENDERER) << endl;
+    cout << "GL version:      " << glGetString(GL_VERSION) << endl;
+    cout << "Desktop size:    " << vidmode->width << "x" << vidmode->height << " pixels" << endl;
+
+    // Get window size. It may start out different from the requested
+    // size, and will change if the user resizes the window.
+    glfwGetWindowSize( window, &width, &height );
+    // Set viewport. This is the pixel rectangle we want to draw into.
+    glViewport( 0, 0, width, height ); // The entire window
+
+    glfwSwapInterval(0); // Do not wait for screen refresh between frames
+
+    // Main loop
+    while(!glfwWindowShouldClose(window))
+    {
+		// Set the clear color and depth, and clear the buffers for drawing
+        glClearColor(0.3f, 0.3f, 0.3f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        /* ---- Rendering code should go here ---- */
+
+		// Swap buffers, i.e. display the image and prepare for next frame.
+        glfwSwapBuffers(window);
+
+		// Poll events (read keyboard and mouse input)
+		glfwPollEvents();
+
+        // Exit if the ESC key is pressed (and also if the window is closed).
+        if(glfwGetKey(window, GLFW_KEY_ESCAPE)) {
+          glfwSetWindowShouldClose(window, GL_TRUE);
+        }
+
+    }
+
+    // Close the OpenGL window and terminate GLFW.
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
