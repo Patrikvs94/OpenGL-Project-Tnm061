9ddd314a0a561c504f4313aaf8976af2d882cafd
PATRIK <patrikvs94@gmail.com>
2016-04-21 10:19:16 +0200

    Segment local translation

--- ballin/Segment.cpp
+++ ballin/Segment.cpp
@@ -1,11 +1,41 @@
 #include "Segment.h"
 
+const float Segment::xsize = 0.6f;
+const float Segment::ysize = 0.6f;
+const float Segment::zsize = 3.0f;
+
+
 Segment::Segment()
 {
-    //ctor
+    //Creating the 3 lanes with constant dimensions
+    Block[0].createBox(xsize, ysize, zsize);
+    Block[1].createBox(xsize, ysize, zsize);
+    Block[2].createBox(xsize, ysize, zsize);
+
+
 }
 
 Segment::~Segment()
 {
     //dtor
 }
+
+void Segment::render()
+{
+
+}
+
+void Segment::localTranslate(matrixStack* p)
+{
+    p->translate(-(xsize + laneMargin), 0.0f, 0.0f);
+    p->push();
+    p->translate((xsize + laneMargin), 0.0f, 0.0f);
+    p->push();
+    p->translate((xsize + laneMargin), 0.0f, 0.0f);
+    p->push();
+}
+
+
+
+
+
--- ballin/Segment.h
+++ ballin/Segment.h
@@ -15,11 +15,19 @@ class Segment : public Element
         void moveToBack(float z);   //Moves the segment to the back of the line
         void reInit();              //Fixes a new version of the segment
 
+        void render();
+        void localTranslate(matrixStack* p);
+
     protected:
 
     private:
         //Array with three blocks of class TriangleSoup
         TriangleSoup Blocks[3];
+
+        //Block dimensions
+        static const float xsize;
+        static const float ysize;
+        static const float zsize;
 };
 
 #endif // SEGMENT_H
